809e2100cda2f1855b405fef55cf90923fedd013
young <yangxu@yeezon.com>
2019-07-30 23:53:47 +0800

    feat: react context

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React, { Component, createContext } from 'react';
 import './App.css';
 
 const BetteryContext = createContext();
+const OnlineContext = createContext();
 
 class Leaf extends Component {
   render () {
     return (
       <BetteryContext.Consumer>
         {
-          battery => <h1>Battery: {battery}</h1>
+          battery => (
+            <OnlineContext.Consumer>
+              {
+                online => (<h1>Battery: {battery}, Online: {online}</h1>)
+              }
+            </OnlineContext.Consumer>
+          )
         }
       </BetteryContext.Consumer>
     );
@@ -22,10 +29,25 @@ class Middle extends Component {
 }
 
 class App extends Component {
+  state = {
+    battery: 30,
+    online: 60
+  }
+
   render () {
+    const { battery, online } = this.state;
+
     return (
-      <BetteryContext.Provider value={60}>
-        <Middle/>
+      <BetteryContext.Provider value={battery}>
+        <OnlineContext.Provider value={online}>
+          <button type="button" onClick={() => this.setState({battery: battery - 1})}>
+            Press-Battery
+          </button>
+          <button type="button" onClick={() => this.setState({online: online - 1})}>
+            Press-Online
+          </button>
+          <Middle/>
+        </OnlineContext.Provider>
       </BetteryContext.Provider>
     );
   }
