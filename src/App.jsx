c04d7b3b3ca2c48f31751af6190fbb8c0dc7177d
Edward-1020 <iezzuyoung@gmail.com>
2019-07-31 23:56:24 +0800

    feat: lazy webpackChunkName suspend 错误边界

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,29 @@
-import React, { Component, createContext } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import './App.css';
 
-const BetteryContext = createContext();
-
-class Leaf extends Component {
-  static contextType = BetteryContext;
-
-  render () {
-    const battery = this.context;
-
-    return (
-        <h1>Battery: {battery}</h1>
-    );
-  }
-}
-
-class Middle extends Component {
-  render () {
-    return <Leaf/>;
-  }
-}
+const About = lazy(() => import(/* webpackChunkName: "about" */'./About.jsx'));
 
 class App extends Component {
   state = {
-    battery: 30
+    hasError: false
+  }
+
+  static getDerivedStateFromError() {
+    return {
+      hasError: true
+    }
   }
 
   render () {
-    const { battery } = this.state;
-
+    if (this.state.hasError) {
+      return (<div>error</div>);
+    }
     return (
-      <BetteryContext.Provider value={battery}>
-          <button type="button" onClick={() => this.setState({battery: battery - 1})}>
-            Press-Battery
-          </button>
-          <Middle/>
-      </BetteryContext.Provider>
+      <div>
+        <Suspense fallback={<div>111</div>}>
+          <About/> 
+        </Suspense>
+      </div>
     );
   }
 }
