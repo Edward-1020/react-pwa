db7dade430bc6c873c8342cde42f6cf7803d71ce
Edward-1020 <iezzuyoung@gmail.com>
2019-08-04 23:38:26 +0800

    feat: 不使用hooks组件

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,42 @@ import React, { Component, useState } from 'react';
 
 class App2 extends Component {
   state = { 
-    count: 0,  
+    count: 0,
+    size: {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight      
+    }  
   };
+  onResize = () => {
+    this.setState({
+      size: {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight
+      }
+    });
+  }
   componentDidMount () {
     document.title = this.state.count;
+
+    window.addEventListener('resize', this.onResize, false);
+  }
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.onResize, false);
   }
   componentDidUpdate () {
     document.title = this.state.count;
+
+    window.addEventListener('resize', this.onResize, false);
   }
   render() {
-    const { count } = this.state;
+    const { count, size } = this.state;
 
     return ( 
       <button
         type="button"
         onClick={() => {this.setState({count: count + 1})}}>
           Click ({count})
+          size: {size.width * size.height}
       </button>
     );
   }
