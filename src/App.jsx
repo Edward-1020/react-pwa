acc57b6fe1a4b998f67ab8f948c220a38cd088d7
Edward-1020 <iezzuyoung@gmail.com>
2019-08-06 22:58:04 +0800

    feat: useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import React, { Component, useState, createContext } from 'react';
+import React, {
+  Component,
+  useState,
+  createContext,
+  useContext
+} from 'react';
 
 const CountContext = createContext();
 
@@ -10,6 +15,26 @@ class Foo extends Component {
       </CountContext.Consumer>
     );
   }
+}
+
+class Bar extends Component {
+  static contextType = CountContext;
+
+  render() {
+    const count = this.context;
+    return (
+      <h1>{count}</h1>
+    );
+  }
+}
+
+
+function Counter () {
+  const count = useContext(CountContext)
+
+  return (
+    <h1>{count}</h1>
+  )
 }
 
 function App (props) {
@@ -24,6 +49,8 @@ function App (props) {
       </button>
       <CountContext.Provider value={count}>
         <Foo/>
+        <Bar/>
+        <Counter/>  
       </CountContext.Provider>
     </div>
   );
