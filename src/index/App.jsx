7f6dfefc7382a10fe45f53b3cec503a3f0b08adf
Edward-1020 <iezzuyoung@gmail.com>
2019-08-16 08:18:03 +0800

    feat: init

---
+++ src/index/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { connect } from 'react-redux';
+
+function App (props) {
+
+}
+
+export default connect(
+    function mapStateToProps (state) {
+    },
+    function mapDispatchToProps (dispatch) {
+    }
+)(App);
