739544c95f4dfa73a09b1228c372eb715ad2b9ec
Edward-1020 <iezzuyoung@gmail.com>
2019-08-20 23:45:41 +0800

    feat: useCallback

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,6 +1,10 @@
 import React,{
-    useCallback
+    useCallback,
+    useMemo
 } from 'react';
+import {
+    bindActionCreators
+} from 'redux';
 import './App.css';
 import { connect } from 'react-redux';
 
@@ -10,7 +14,17 @@ import HighSpeed from './HighSpeed';
 import Journey from './Journey';
 import Submit from './Submit';
 
+import {
+    exchangeFromTo,
+    showCitySelector
+} from './actions';
+
 function App(props) {
+    const {
+        from,
+        to,
+        dispatch
+    } = props;
 
     const onBack = useCallback(
         () => {
@@ -19,24 +33,39 @@ function App(props) {
         [],
     )
 
+    const cbs = useMemo(() => {
+        return bindActionCreators({
+            exchangeFromTo,
+            showCitySelector
+        }, dispatch);
+    }, [])
+    
     return (
         <div>
             <div className="header-wrapper">
                 <Header title="火车票" onBack={onBack} />
             </div>
-            <Journey />
-            <DepartDate />
-            <HighSpeed />
-            <Submit />
+            <form className="form">
+                <Journey
+                    from={from}
+                    to={to}
+                    {...cbs}
+                    />
+                <DepartDate />
+                <HighSpeed />
+                <Submit />
+            </form>
         </div>
     );
 }
 
 export default connect(
     function mapStateToProps(state) {
-        return {};
+        return state;
     },
     function mapDispatchToProps(dispatch) {
-        return {};
+        return {
+            dispatch
+        };
     }
 )(App);
